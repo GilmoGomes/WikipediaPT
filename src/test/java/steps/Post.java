9db15d9ae595b88dd1eacd026bf0f47b148023c0
gilmo.gomes <gilmo.gomes@gmail.com>
2021-03-15 23:21:36 -0300

    Concluimos os relatorios, o Runner e criamos a task smokeTest

--- src/test/java/steps/Post.java
+++ src/test/java/steps/Post.java
@@ -1,5 +1,8 @@
 package steps;
 
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Entao;
 import cucumber.api.java.pt.Quando;
@@ -17,7 +20,7 @@ public class Post extends Base {
     }
 
     private Base base;
-
+/*
     @Dado("^que acesso o Wikipedia em Portugues$")
     public void queAcessoOWikipediaEmPortugues() {
         base.driver.get(base.url); //Abre o navegador no seite alvo (extentendo da base)
@@ -31,6 +34,26 @@ public class Post extends Base {
 
     @Entao("^Exibe a expressao \"([^\"]*)\" no titulo da guia$")
        public void exibeAExpressaoNoTituloDaGuia(String produto) {
+
+        //WebDriverWait espera = new WebDriverWait(base.driver, 30);
+        //okespera.until(ExpectedConditions.titleIs(produto + " - Wikipédia, a enciclopédia livre"));
+
+        assertTrue(base.driver.getTitle().contains(produto));
+    }
+*/
+    @Given("^que acesso o Wikipedia em Portugues$")
+    public void queAcessoOWikipediaEmPortugues() {
+        base.driver.get(base.url); //Abre o navegador no seite alvo (extentendo da base)
+
+    }
+
+    @When("^pesquiso por \"([^\"]*)\"$")
+    public void pesquisoPor(String produto) {
+        base.driver.findElement(By.id("searchInput")).sendKeys(produto + Keys.ENTER);
+    }
+
+    @Then("^Exibe a expressao \"([^\"]*)\" no titulo da guia$")
+    public void exibeAExpressaoNoTituloDaGuia(String produto) {
 
         //WebDriverWait espera = new WebDriverWait(base.driver, 30);
         //okespera.until(ExpectedConditions.titleIs(produto + " - Wikipédia, a enciclopédia livre"));

fecefd3f8e027298051fdfd2d00887aea616346f
gilmo.gomes <gilmo.gomes@gmail.com>
2021-03-15 23:41:38 -0300

    Concluimos os relatorios, o Runner e criamos a task smokeTest
    Dnovo

---
+++ target/relatorioSimples/report.js
@@ -0,0 +1,95 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Post.feature");
+formatter.feature({
+  "comments": [
+    {
+      "line": 2,
+      "value": "# Funcionalidade: Consulta"
+    },
+    {
+      "line": 3,
+      "value": "#Cenario: Consulta Ovo de Pascoa"
+    },
+    {
+      "line": 4,
+      "value": "#Dado que acesso o Wikipedia em Portugues"
+    },
+    {
+      "line": 5,
+      "value": "#Quando pesquiso por \"Ovo de Páscoa\""
+    },
+    {
+      "line": 6,
+      "value": "#Entao Exibe a expressao \"Ovo de Páscoa\" no titulo da guia"
+    }
+  ],
+  "line": 10,
+  "name": "Consulta",
+  "description": "",
+  "id": "consulta",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 1672819200,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 11,
+  "name": "Consulta Ovo de Pascoa",
+  "description": "",
+  "id": "consulta;consulta-ovo-de-pascoa",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 12,
+  "name": "que acesso o Wikipedia em Portugues",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 13,
+  "name": "pesquiso por \"Ovo de Páscoa\"",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 14,
+  "name": "Exibe a expressao \"Ovo de Páscoa\" no titulo da guia",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Post.queAcessoOWikipediaEmPortugues()"
+});
+formatter.result({
+  "duration": 2029457500,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Ovo de Páscoa",
+      "offset": 14
+    }
+  ],
+  "location": "Post.pesquisoPor(String)"
+});
+formatter.result({
+  "duration": 1311735600,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Ovo de Páscoa",
+      "offset": 19
+    }
+  ],
+  "location": "Post.exibeAExpressaoNoTituloDaGuia(String)"
+});
+formatter.result({
+  "duration": 6049900,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 844586900,
+  "status": "passed"
+});
+});
